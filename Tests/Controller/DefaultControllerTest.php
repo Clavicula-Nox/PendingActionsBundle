dbfb4fca87b7041706872bb2d00f6fdae6df243a
Adrien Lochon <alochon@chorus.fr>
2016-02-02 16:24:36 +0100

    Fix travis

---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ClaviculaNox\CNPendingActionsBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+
+    }
+}
