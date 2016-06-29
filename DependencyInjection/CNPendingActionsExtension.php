24297c1fa26d3fa7d42adebdb5c7ff038180e7fe
Adrien Lochon <alochon@chorus.fr>
2016-06-29 15:51:20 +0200

    Unused "use" statements

--- DependencyInjection/CNPendingActionsExtension.php
+++ DependencyInjection/CNPendingActionsExtension.php
@@ -4,9 +4,7 @@ namespace ClaviculaNox\CNPendingActionsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class CNPendingActionsExtension extends Extension
