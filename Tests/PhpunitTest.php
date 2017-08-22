6add1eca8671db9e319c5d526d09a10bac4da59d
Adrien Lochon <alochon@condenast.fr>
2017-08-22 17:41:35 +0200

    Phpunit unit test

--- Tests/PhpunitTest.php
+++ Tests/PhpunitTest.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * This file is part of the PendingActionsBundle.
- *
- * (c) Adrien Lochon <adrien@claviculanox.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of the PendingActionsBundle.
+*
+* (c) Adrien Lochon <adrien@claviculanox.io>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace ClaviculaNox\PendingActionsBundle\Tests;
 
