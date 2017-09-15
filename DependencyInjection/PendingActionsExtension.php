3f0c42ebe56cc4ab0bb33de8be6117eda45dd6bd
Adrien Lochon <alochon@condenast.fr>
2017-09-15 12:50:05 +0200

    Change order

--- DependencyInjection/PendingActionsExtension.php
+++ DependencyInjection/PendingActionsExtension.php
@@ -13,8 +13,8 @@ namespace ClaviculaNox\PendingActionsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Class PendingActionsExtension
