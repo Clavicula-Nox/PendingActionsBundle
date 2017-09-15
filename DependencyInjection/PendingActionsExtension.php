17c9ae4b5b7115ecf730d440b44b2f2eceac307a
Adrien Lochon <alochon@condenast.fr>
2017-09-15 12:21:31 +0200

    Moved services.xml to services.yaml

--- DependencyInjection/PendingActionsExtension.php
+++ DependencyInjection/PendingActionsExtension.php
@@ -13,8 +13,8 @@ namespace ClaviculaNox\PendingActionsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * Class PendingActionsExtension
@@ -27,9 +27,7 @@ class PendingActionsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $locator = new FileLocator(__DIR__ . '/../Resources/config/');
-        $loader  = new XmlFileLoader($container, $locator);
-
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config/'));
+        $loader->load('services.yaml');
     }
 }
